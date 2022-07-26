4abfcbace445de51a29dca7fb4f180e68eab7625
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-07-26 17:50:58 -0400

    updated

--- filemaker.sh
+++ filemaker.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
 user=`whoami`
-current=`ls -1 | wc -l`
+current=`ls -1 | grep -P "$user" | wc -l`
 numfiles=25
 goal=$(($current+$numfiles))
 
 filenamearray=`ls -1 | grep -P "${user}[0-9]+"`
+#echo $filenamearray
 filenamecount=`ls -1 | grep -P "${user}[0-9]+" | wc -l`
-filenumberarray=()
-
-# echo $filenamearray
-
 echo $filenamecount
+filenumberarray=()
 
 #echo ${#filenamearray[@]}
 
-#created another comment for demo
+counter=00
+
+for i in ${filenamearray[@]}; do
+  echo $i
+  [[ "$i" =~ (${user})([0-9]+) ]] #regex sorting for user and index
+  if (( ${BASH_REMATCH[2]#0} == $counter )); then # the #0 forces decimal interpretation instead of octal
+    ((counter=counter+1))
+  echo $counter
+  else
+    echo "bash_rematch2 does not equal counter"
+    mkdir "${user}0${counter}"
+  fi
+  filenumberarray+=(${BASH_REMATCH[2]}) #adds the index to the filenumberarray
+done
+
+echo ${filenumberarray[@]}
 
 
-# for i in ${filenamearray[@]}; do
-#   [[ "$i" =~ (${user})([0-9]+) ]]
-#   # echo "${BASH_REMATCH[2]}"
-#   filenumberarray+=(${BASH_REMATCH[2]})
-# done
 
-# echo $filenumberarray
-
-# max=${ar[0]}
-# for n in "${ar[@]}" ; do
+# max=${filenumberarray[0]}
+# for n in "${filenumberarray[@]}" ; do
 #     ((n > max)) && max=$n
 # done
 # echo $max
 
-# if (( $current < 10 )); then
-#   until (( $current == 10 )); do
-#   mkdir "${user}0${current}"
-#   ((current=current+1))
-#   done
-# fi
+if (( $current < 10 )); then
+  until (( $current == 10 )); do
+  mkdir "${user}0${current}"
+  ((current=current+1))
+  done
+fi
 
 # while (( $current != $goal )); do
 #   # echo "${user}${current}"
