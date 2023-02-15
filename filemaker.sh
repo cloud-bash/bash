d6e58b852847cca51302929731264fc25beaa797
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2023-02-15 17:15:11 -0500

    Update filemaker.sh

--- filemaker.sh
+++ filemaker.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 #makes number of directories use inputs as an argument
-#if range of directories (00-quantity) is missing any numbers, remake them
+#if range of directories (1-quantity) is missing any numbers, remake them
 #repeating the script run with the same quantity each time will only create quantity maximum
 #for more directories increase the quantity
-#only works for 01-99
+
+#script use: ./filemaker.sh [number of files to make] [name of files]
+#example: ./filemaker 10 test
+#makes file1, file2...file10
 
 quantity=$1
+name=$2
 
 for i in $(seq 1 "$quantity"); do
-  if [[ -e "$USER""0$i" || -e "$USER""$i" ]]; then
+  if [[ -e "$name""$i" || -e "$name""$i" ]]; then
     if [[ $i -lt 10 ]]; then
-      echo "$USER""0$i"" already exists"
+      echo "$name""$i"" already exists"
     else
-      echo "$USER""$i"" already exists"
+      echo "$name""$i"" already exists"
     fi
   else
     if [[ $i -lt 10 ]]; then
-      mkdir "$USER""0$i"
+      touch "$name""$i"
     else
-      mkdir "$USER""$i"
+      touch "$name""$i"
     fi
   fi
 done
